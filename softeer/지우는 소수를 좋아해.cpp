14dc414b3e7fda2ad30bc7b173739c11456c2116
TMOBMO <wayway910@gmail.com>
2023-02-13 00:35:11 +0900

    add

--- softeer/지우는 소수를 좋아해.cpp
+++ softeer/지우는 소수를 좋아해.cpp
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////
+// solve-1
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -71,5 +74,84 @@ int main() {
   }
 
   cout << prime(dp[1] + 1) << '\n';
+  return 0;
+}
+
+/////////////////////////////////////////////////
+// solve-2
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+map<int, vector<pair<int, int>>> connect;
+vector<int> dp;
+
+
+void solve() {
+  auto comp = [](pair<int, int> a, pair<int, int> b) {
+    return a.second > b.second;
+  };
+
+  priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+
+  pq.push({ 1, 0 });
+  dp[1] = 0;
+
+  while (!pq.empty()) {
+    auto t = pq.top(); pq.pop();
+
+    int cur = t.first;
+    int cur_cost = t.second;
+
+    for (int i = 0; i < connect[cur].size(); ++i) {
+      int next = connect[cur][i].first;
+      int next_cost = connect[cur][i].second;
+
+      int cost = max(cur_cost, next_cost);
+      if (dp[next] <= cost) continue;
+      dp[next] = cost;
+      pq.push({ next, cost });
+    }
+  }
+}
+
+bool isPrime(long long n) {
+  if (n % 2 == 0) return false;
+  for (long long i = 2; i < n; ++i) {
+    if (i * i > n) break;
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+long long prime(long long n) {
+  while (1) {
+    if (isPrime(n)) return n;
+    ++n;
+  }
+  return 0;
+}
+
+int main() {
+  cin >> N >> M;
+
+  for (int i = 0; i < M; ++i) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    connect[a].push_back({ b, c });
+    connect[b].push_back({ a, c });
+  }
+
+  dp.resize(N + 1, 1e9);
+
+  solve();
+
+  cout << prime(dp[N] + 1) << '\n';
   return 0;
 }
